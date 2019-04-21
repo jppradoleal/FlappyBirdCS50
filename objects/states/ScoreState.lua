005588370f56bf9e6099d5b7a044892f74f72fce
ImMurderer <joaopedro0128@hotmail.com>
2019-04-21 20:16:11 -0300

    feat: added random gaps and space between pipes, medals and (P)ause feature

--- objects/states/ScoreState.lua
+++ objects/states/ScoreState.lua
@@ -2,6 +2,7 @@ ScoreState = Class{__includes = BaseState}
 
 function ScoreState:enter(params)
     self.score = params.score
+    self.badge = nil
 end
 
 function ScoreState:update(dt)
@@ -19,4 +20,22 @@ function ScoreState:render()
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
 
     love.graphics.printf('Press Enter to play again!', 0, 136, VIRTUAL_WIDTH, 'center')
+
+    if self.score > 4 and self.score < 10 then
+        self.badge = badges['first']
+    elseif self.score > 9 and self.score< 50 then
+        self.badge = badges['second']
+    elseif self.score>49 and self.score < 100 then
+        self.badge = badges['third']
+    elseif self.score>99 and self.score < 150 then
+        self.badge = badges['forth']
+    elseif self.score>149 then
+        self.badge = badges['fifth'] 
+    end
+
+    if self.badge then
+        love.graphics.draw(self.badge, VIRTUAL_WIDTH / 2 - 8, 172)
+        love.graphics.setFont(smallFont)
+        love.graphics.printf("You earned a badge!", 0, 192, VIRTUAL_WIDTH, 'center')
+    end
 end
